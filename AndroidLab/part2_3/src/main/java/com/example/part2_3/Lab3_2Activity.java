d2657f1b3cc4c8a121e69c8de64875e0a66e58f7
YongBae Kim <73388879+kybw0123@users.noreply.github.com>
2021-02-08 21:58:36 +0900

    xml과 java로 코드를 작성하여 버튼을 생성함 xml 디자인 모드에서 버튼의 위치를 수정하여 확인함

--- AndroidLab/part2_3/src/main/java/com/example/part2_3/Lab3_2Activity.java
+++ AndroidLab/part2_3/src/main/java/com/example/part2_3/Lab3_2Activity.java
@@ -1,18 +1,14 @@
-package com.example.androidlab;
+package com.example.part2_3;
 
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class Lab3_2Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
-        setContentView(R.layout.activity_main);
-
-
+        setContentView(R.layout.activity_lab3_2);
     }
 }
